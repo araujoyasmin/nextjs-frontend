4a2b3587750d4fdcf877f161be7a699d743ad7c0
araujoyasmin <yasaraujo@icloud.com>
2024-02-17 23:02:49 -0300

    :tada: front com nextjs

---
+++ src/app/pagina1/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+function Pagina1(){
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <Button variant="contained" color="primary"> clique aqui</Button>
+        </div>
+    )
+}
+
+export default Pagina1;
